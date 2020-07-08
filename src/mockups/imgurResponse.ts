fcbe074405c3dd1ff5c86ba8e6ac58542050852f
erifranck <erifranjose@hotmail.com>
2020-07-08 08:27:45 -0500

    create mockups

---
+++ src/mockups/imgurResponse.ts
@@ -0,0 +1,24 @@
+export const getImagesResponse = {
+      data: [
+            {
+                  "id": "QJa0R6q",
+                  "title": "Shiprock - New Mexico [OC][1080x644]",
+                  "type": "image/jpeg",
+                  "link": "http://i.imgur.com/QJa0R6q.jpg",
+            },
+            {
+
+                  "id": "QJa0R6q",
+                  "title": "Shiprock - New Mexico [OC][1080x644]",
+                  "type": "image/jpeg",
+                  "link": "http://i.imgur.com/QJa0R6q.jpg",
+            },
+            {
+
+                  "id": "QJa0R6q",
+                  "title": "Shiprock - New Mexico [OC][1080x644]",
+                  "type": "image/jpeg",
+                  "link": "http://i.imgur.com/QJa0R6q.jpg",
+            }
+      ]
+}
