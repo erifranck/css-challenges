9c8cf001a1a3f0d2f97303695dadf0d6c7e24511
erifranck <erifranjose@hotmail.com>
2020-07-13 16:47:00 -0500

    create card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,1 +1,54 @@
-export {} 
+import React from 'react';
+import './Card.scss';
+import { classNames } from '../../utils/classNames';
+
+interface Props {
+      imgUrl?: string;
+      count?: string;
+      flip: boolean;
+      onStart: () => void
+      onFinish: () => void
+}
+export const Card: React.FC<Props> = (props) => {
+      const flipClasses = classNames({
+            'card-flip': true,
+            flipped: props.flip,
+      })
+      return (
+            <div className="card-container">
+                  <div className={flipClasses}>
+                        <div className="card-front">
+                              <div className="card-body">
+                                    <div>
+                                          <h1>NIVEL 1</h1>
+                                          <p>30 minutos para completar el reto</p>
+                                    </div>
+                              </div>
+                              <div className="card-footer">
+                                    <button
+                                     className="card-front-button"
+                                     onClick={props.onStart}
+                                    > 
+                                          START 
+                                    </button>
+                              </div>
+                        </div>
+                        <div className="card-back">
+                              <div className="card-body">
+                                    <img src={props.imgUrl} alt=""/>
+                                    <p>TIEMPO RESTANTE</p>
+                                    <p>{props.count}</p>
+                              </div>
+                              <div className="card-footer">
+                                    <button
+                                     className="card-back-button"
+                                     onClick={props.onFinish}
+                                    > 
+                                          FINISH
+                                    </button>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      )
+}
