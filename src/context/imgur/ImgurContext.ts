7497638d5d81568d05b8303672bda07dae78dc73
erifranck <erifranjose@hotmail.com>
2020-07-13 16:43:19 -0500

    create imgur context

--- src/context/imgur/ImgurContext.ts
+++ src/context/imgur/ImgurContext.ts
@@ -2,21 +2,23 @@ import { ImgurData } from './../../types/imgur';
 import { ProviderCreator } from './../contextInjector';
 import { ManageContext } from './../../types/stateManagement';
 import React from 'react'
-import { imgurReducer } from './ImgurReducer';
+import { imgurReducer, imgurPersistor } from './ImgurReducer';
 
 export interface ImgurState {
       fetch: boolean;
       data?: ImgurData[];
+      token?: string;
 }
 
 export type ImgurAction = {
       type: string;
       data?: ImgurData[];
+      token?: string;
 }
 
 export type ImgurContext = ManageContext<ImgurState, ImgurAction>
 
-export const INITIAL_STATE = {
+export const INITIAL_STATE = imgurPersistor.get() || {
       fetch: false,
 }
 
