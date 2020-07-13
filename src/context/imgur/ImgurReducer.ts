b32b5e3b778d7c391ffea5377aa337987f98d7f1
erifranck <erifranjose@hotmail.com>
2020-07-13 16:43:56 -0500

    create imgur context reducer

--- src/context/imgur/ImgurReducer.ts
+++ src/context/imgur/ImgurReducer.ts
@@ -1,20 +1,29 @@
 import { INITIAL_STATE, ImgurState, ImgurAction } from './ImgurContext';
-import { GET_IMAGES, GET_IMAGES_SUCCESS } from './ImgurActions';
+import { GET_IMAGES, GET_IMAGES_SUCCESS, SET_TOKEN } from './ImgurActions';
+import { Persistor } from '../../utils/persistor';
 
+export const imgurPersistor = Persistor<ImgurState>('imgur');
 export function imgurReducer(state = INITIAL_STATE, action: ImgurAction) : ImgurState {
-      switch(action.type) {
-            case GET_IMAGES:
-                  return {
-                        ...state,
-                        fetch: true
-                  };
-            case GET_IMAGES_SUCCESS:
-                  return {
-                        ...state,
-                        fetch: false,
-                        data: action.data
-                  };
-            default: 
-                  return state;
-      }
+      return imgurPersistor.set((() => {
+            switch(action.type) {
+                  case GET_IMAGES:
+                        return {
+                              ...state,
+                              fetch: true
+                        };
+                  case GET_IMAGES_SUCCESS:
+                        return {
+                              ...state,
+                              fetch: false,
+                              data: action.data
+                        };
+                  case SET_TOKEN:
+                        return {
+                              ...state,
+                              token: action.token
+                        };
+                  default: 
+                        return state;
+            }
+      })())
 }
