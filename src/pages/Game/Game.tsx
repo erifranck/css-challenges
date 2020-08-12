735d716415c1f3e77ccd6bc32cd4dbc0f276bccb
erifranck <erifranjose@hotmail.com>
2020-08-11 20:12:51 -0500

    add new pages

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
-import { useImgur } from '../../context/imgur/ImgurHooks';
+import { useImgur, useSetScore } from '../../context/imgur/ImgurHooks';
 import { ImgurData } from '../../types/imgur';
 import { Card } from '../../components/Card/Card';
+import { useCounter, CounterFunction } from '../../utils/countDown';
+import { randomSelector } from '../../utils/randomSelector';
 import './Game.scss';
-import { useCounter } from '../../utils/countDown';
+import { NUMBER_OF_LEVELS, BASE_TIME, BASE_SCORE } from '../../utils/constants';
 
-
+interface LevelSelected {
+      time: number;
+      level: number
+}
 export const Game = () => {
       const [state, getImages] = useImgur();
       const [card, setCard] = React.useState<ImgurData>();
+      const [counter, setCounter] = React.useState<CounterFunction>();
+      const [levelSelected, setLevel] = React.useState<LevelSelected>();
       const [count, starCountDown] = useCounter();
+      const setScore = useSetScore();
       React.useEffect( () => {
             getImages()
       }, []);
       return (
             <div className="game-wrapper">
-                  <Card
-                        flip={!!card}
-                        imgUrl={card?.link}
-                        count={count}
-                        onStart={() => {
-                              starCountDown();
-                              setCard(state.data && state.data[0])     
-                        }}
-                        onFinish={() => {
-                             setCard(undefined) 
-                        }}
-                  />
+                  {!(card && levelSelected) ? 
+                        Array.from({length: NUMBER_OF_LEVELS}).map((intem, index) => {
+                              const timeLimit = NUMBER_OF_LEVELS * BASE_TIME / (index + 1)
+                              return  (
+                                    <Card
+                                          flip={!!card}
+                                          imgUrl={card?.link}
+                                          count={count}
+                                          time={timeLimit}
+                                          level={index + 1}
+                                          onStart={() => {
+                                                setLevel({time: timeLimit, level: index + 1})
+                                                setCounter(starCountDown(timeLimit));
+                                                setCard(state.data && state.data[randomSelector(state.data.length)])     
+                                          }}
+                                          onFinish={() => {}}
+                                    />
+                              )
+                        }): (
+                              <Card
+                                    flip={!!card}
+                                    imgUrl={card?.link}
+                                    count={count}
+                                    time={levelSelected?.time}
+                                    level={levelSelected?.level}
+                                    onStart={() => {}}
+                                    onFinish={() => {
+                                          setScore(levelSelected.level * BASE_SCORE)
+                                          counter?.stopCounter()
+                                          setCard(undefined) 
+                                    }}
+                              />
+                        )
+            }
             </div>
       )
 }
