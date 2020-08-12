fc800e387b990ce79a790b9b6371f435108bd55e
erifranck <erifranjose@hotmail.com>
2020-08-11 20:15:13 -0500

    add countdown method

--- src/utils/countDown.ts
+++ src/utils/countDown.ts
@@ -1,28 +1,38 @@
 import { useState } from 'react';
+
+export interface CounterFunction {
+      stopCounter: () => void;
+}
+
 function counter(delay: number = 15) {
       return new Date().getTime() + (delay * 60 * 1000 );
 }
 
 function countFormatter(distance: number) {
       const minutes = Math.floor(distance % (1000 * 60 * 60) / ( 1000 * 60 ));
-      const seconds = Math.floor(distance % (1000 * 60) / 1000);
+      const seconds = Math.ceil(distance % (1000 * 60) / 1000);
       const milliseconds = distance % 1000;
       return `${minutes}:${seconds}:${milliseconds}`;
 }
 
-export const useCounter = (delay = 15): [string, () => void] => {
+export const useCounter = (): [string, (delay: number) => CounterFunction] => {
       const [state, setstate] = useState('00:00:000')
       return [
             state,
-            () => {
+            (delay = 15) => {
                   const countDown = counter(delay);
                   const interval = setInterval(() => {
                         const distance = countDown - new Date().getTime();
-                        setstate(countFormatter(distance))
-                        if(distance === 0) {
+                        if(distance <= 0) {
                               clearInterval(interval);
                         }
+                        setstate(countFormatter(distance))
                   })
+                  return {
+                        stopCounter: () => {
+                              clearInterval(interval)
+                        }
+                  }
             }
       ]      
 }
